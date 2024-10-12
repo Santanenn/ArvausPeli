aa233296a7e3e1c82db313013ee8cd4c938cd528
Eveliina <evesantanen@gmail.com>
2024-10-12 18:36:42 +0300

    1
    Ohjelma kysyy nimeä ja loppuu kun vastaa oikein.

--- Arvaus peli/src/App.java
+++ Arvaus peli/src/App.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner in = new Scanner(System.in);
+        String input;
+        String oikeaNimi = ("Kalevi");
+
+        do{
+            System.out.println("Arvaa nimi.");
+            input = in.nextLine();
+
+            if(input.equals(oikeaNimi))
+            {
+                System.out.println("Oikein!");
+                break;
+            }
+        }
+        while(true);
+
+
     }
 }
